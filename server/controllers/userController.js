3be93c0a96362b019c3fa03448883fd5ac52c2bb
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-16 10:05:25 +0530

    created routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
+import imagekit from "../configs/imagekit";
 import User from "../models/User";
-
+import fs from "fs";
 //Get user data using userId
 export const getUserData = async (req, res) => {
   try {
@@ -17,11 +18,11 @@ export const getUserData = async (req, res) => {
 export const updateUserData = async (req, res) => {
   try {
     const { userId } = req.auth();
-    const { username, bio, location, full_name } = req.body;
+    let { username, bio, location, full_name } = req.body;
     const tempUser = await User.findById(userId);
     !username && (username = tempUser.username);
     if (tempUser.username !== username) {
-      const user = User.findOne({ username });
+      const user = await User.findOne({ username });
       if (user) {
         username = tempUser.username;
       }
@@ -34,6 +35,106 @@ export const updateUserData = async (req, res) => {
     };
     const profile = req.files.profile && req.files.profile[0];
     const cover = req.files.cover && req.files.cover[0];
+    if (profile) {
+      const buffer = fs.readFileSync(profile.path);
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+      });
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          { quality: "auto" },
+          { format: "webp" },
+          { width: "512" },
+        ],
+      });
+      updateData.profile_picture = url;
+    }
+    if (cover) {
+      const buffer = fs.readFileSync(cover.path);
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+      });
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          { quality: "auto" },
+          { format: "webp" },
+          { width: "1280" },
+        ],
+      });
+      updateData.cover_photo = url;
+    }
+    const user = await User.findByIdAndUpdate(userId, updateData, {
+      new: true,
+    });
+    res.json({ success: true, user, message: "Profile updated successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+//Find users using username,email,location,name
+export const discoverUsers = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { input } = req.body;
+    const allUsers = await User.find({
+      $or: [
+        { username: new RegExp(input, "i") },
+        { email: new RegExp(input, "i") },
+        { full_name: new RegExp(input, "i") },
+        { location: new RegExp(input, "i") },
+      ],
+    });
+    const filteredUsers = allUsers.filter((user) => user._id !== userId);
+    res.json({ success: true, users: filteredUsers });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+//Follow user
+export const followUser = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+    const user = await User.findById(userId);
+    if (user.following.includes(id)) {
+      return res.json({
+        success: false,
+        message: "You are already following this user",
+      });
+    }
+    user.following.push(id);
+    await user.save();
+    const toUser = await User.findById(id);
+    toUser.followers.push(userId);
+    await toUser.save();
+    res.json({ success: true, message: "Now you are following this user" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+//Unfollow user
+export const unfollowUser = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+    const user = await User.findById(userId);
+    user.following = user.following.filter((user) => user !== id);
+    await user.save();
+    const toUser = await User.findById(id);
+    toUser.followers = toUser.followers.filter((user) => user !== userId);
+    await toUser.save();
+    res.json({
+      success: true,
+      message: "You are no longer following this user",
+    });
+    res.json({
+      success: true,
+      message: "Now you are following this user",
+    });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
